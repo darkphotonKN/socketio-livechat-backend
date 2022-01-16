57b15bb023f3445d5b562d3b7c3ea20ed6a01fa5
Kranti <darkphoton20@gmail.com>
2022-01-16 15:19:41 +0800

    Setup socket.io backend.

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const axios = require('axios');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  transports: ['polling'],
+  cors: {
+    // the server that will be calling our socket server
+    origin: '*',
+  },
+});
+
+const PORT = 4040;
+
+io.on('connection', async (socket) => {
+  console.log(`User ${socket.id} has connected..`);
+
+  const { data } = await axios.get(
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+
+  data.forEach((item, index) => {
+    setTimeout(
+      () => socket.emit(item?.body, 'message from server'),
+      Math.random() * index * 1000
+    );
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User ${socket.id} has disconnected..`);
+  });
+});
+
+app.use(cors());
+
+server.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
+
+// harm in terms of how we think of ourselves.
